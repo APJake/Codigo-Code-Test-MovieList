1b7f8a92dfdf02fc04b99813394b23a4aa0016f8
apjake <aung.amk.edu@gmaiil.com>
2022-04-19 14:22:27 +0630

    module repair

--- common/src/main/java/com/apjake/codetestmovielist/common/util/UniMapper.kt
+++
@@ -1,5 +0,0 @@
-package com.apjake.codetestmovielist.common.util
-
-interface UniMapper<FROM, TO> {
-    fun map(data: FROM): TO
-}
--- network/src/main/java/com/apjake/codetestmovielist/network/response/MovieListResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.apjake.codetestmovielist.network.response
-
-data class MovieListResponse(
-    val page: Int?,
-    val results: List<Movie>?,
-    val total_pages: Int?,
-    val total_results: Int?
-)
